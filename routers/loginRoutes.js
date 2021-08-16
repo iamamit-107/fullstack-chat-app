07cbff75dfce7f2927d65fab784c051e566219ad
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-16 20:35:18 +0600

    avatar upload operation with multer

--- routers/loginRoutes.js
+++ routers/loginRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { getLogin } = require("../controllers/loginController");
 const decorateHtml = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUploads");
 
 const router = express.Router();
 
 router.get("/", decorateHtml("Login"), getLogin);
+router.post("/", avatarUpload);
 
 module.exports = router;
