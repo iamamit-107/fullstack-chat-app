a05b7921072bea29cef74ee1dd3cee9930036416
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-17 11:48:53 +0600

    add user controller added

--- routers/loginRoutes.js
+++ routers/loginRoutes.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const { getLogin } = require("../controllers/loginController");
 const decorateHtml = require("../middlewares/common/decorateHtmlResponse");
-const avatarUpload = require("../middlewares/users/avatarUploads");
-const {
-  addUserValidators,
-  addUserValidationHandler,
-} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
-// users page
+// Login page
 router.get("/", decorateHtml("Login"), getLogin);
-
-// add user
-router.post("/", avatarUpload, addUserValidators, addUserValidationHandler);
 
 module.exports = router;
--- routers/usersRoutes.js
+++ routers/usersRoutes.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const { getUsers } = require("../controllers/usersController");
+const { getUsers, addUser } = require("../controllers/usersController");
 const decorateHtml = require("../middlewares/common/decorateHtmlResponse");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
+const avatarUpload = require("../middlewares/users/avatarUploads");
 
 const router = express.Router();
 
+// Users page
 router.get("/", decorateHtml("Users"), getUsers);
+
+// add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
