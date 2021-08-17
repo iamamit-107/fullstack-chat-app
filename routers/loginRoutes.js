90828d21c8bf2d4a2bc4ba18a75432e9065d1829
iamamit-107 <ahmed.amit107@gmail.com>
2021-08-17 11:10:42 +0600

    user validation and validation handler middleware added to add user route

--- routers/loginRoutes.js
+++ routers/loginRoutes.js
@@ -2,10 +2,17 @@ const express = require("express");
 const { getLogin } = require("../controllers/loginController");
 const decorateHtml = require("../middlewares/common/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUploads");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
+// users page
 router.get("/", decorateHtml("Login"), getLogin);
-router.post("/", avatarUpload);
+
+// add user
+router.post("/", avatarUpload, addUserValidators, addUserValidationHandler);
 
 module.exports = router;
